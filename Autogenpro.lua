e447421f9fb9e33ac6aeeec3b988795c9baded07
NgKhaiVN <giabaohua64@gmail.com>
2025-10-22 09:56:09 +0700

    Update Autogenpro.lua

--- Autogenpro.lua
+++ Autogenpro.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/NgKhaiVN/5b40c1232755baf872c7435bccff6010/raw/a45dcae7a5ec9008f35f96e3c8bc24cd6d443f97/AutoGenpro.lua"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/da85b459d77e4d13ac3d4804f4a99681"))()
